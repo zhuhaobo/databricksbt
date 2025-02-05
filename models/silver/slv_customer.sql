5920a41aea9dd0b75ab777452cf1440cfbdec1a6
Steve Zhu <steve.zhu@fusion5.co.nz>
2025-02-05 00:49:28 +0000

    update config for slv_customer

--- models/silver/slv_customer.sql
+++ models/silver/slv_customer.sql
@@ -3,7 +3,7 @@
     (    
         tags=["silver", "run_daily"],
         unique_key='ID',
-        materialized="view"
+        materialized="incremental"
     ) 
 }}
 
@@ -14,4 +14,8 @@ LAST_NAME,
 concat(FIRST_NAME, ' ', LAST_NAME) as FULL_NAME,
 ingestion_datetime
 from {{ ref('jaffle_shop_customers') }}
+
+{% if is_incremental() %}
+WHERE ingestion_datetime > (SELECT MAX(ingestion_datetime) FROM {{ this }})
+{% endif %}
 
